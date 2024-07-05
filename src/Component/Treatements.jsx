073842eb4a2603f03153762bc080fd1fd6e535b3
salman ansari <salmanansari1409@gmail.com>
2024-07-05 17:58:19 +0530

    addsomedetails

--- src/Component/Treatements.jsx
+++ src/Component/Treatements.jsx
@@ -1,66 +1,70 @@
 import React from 'react'
-import laprscpyIcons from '../assets/icons/laprscpyIcons.png'
+// import laprscpyIcons from '../assets/icons/laprscpyIcons.png'
+import hearticon from '../assets/icons/hearticon.png'
 
 const Treatements = () => {
     return (
         <section class="text-gray-600 body-font">
             <div class="container px-5 py-24 mx-auto">
+                <div className='my-9'>
+                    <h1 className='text-4xl font-bold'>TREATEMENTS</h1>
+                </div>
                 <div class="flex flex-wrap -m-4">
-                    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src={laprscpyIcons}/>
+                    <div class="lg:w-1/4 m-3 md:w-1/2 p-4 w-full shadow">
+                        <a class=" text-blue-600 block relative h-48 rounded overflow-hidden">
+                            <img alt="ecommerce" class="object-cover object-center w-36 h-36 block" src={hearticon} />
                         </a>
                         <div class="mt-4">
+                            <h2 class="text-gray-900 title-font text-lg font-medium">Advance Laparoscopy Surgery</h2>
                             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
                         </div>
                     </div>
-                    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <div class="lg:w-1/4 m-4 md:w-1/2 p-4 w-full shadow">
                         <a class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src={laprscpyIcons}/>
+                            <img alt="ecommerce" class="object-cover object-center w-36 h-36 block" src={hearticon} />
                         </a>
                         <div class="mt-4">
+                            <h2 class="text-gray-900 title-font text-lg font-medium">LASER in General Surgery</h2>
                             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
                         </div>
                     </div>
-                    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <div class="lg:w-1/4 m-4 md:w-1/2 p-4 w-full shadow">
                         <a class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src={laprscpyIcons}/>
+                            <img alt="ecommerce" class="object-cover object-center w-36 h-36 block" src={hearticon} />
                         </a>
                         <div class="mt-4">
+                            <h2 class="text-gray-900 title-font text-lg font-medium">Bariatric and Weight Loss Surgery </h2>
                             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
                         </div>
                     </div>
-                    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <div class="lg:w-1/4 m-4 md:w-1/2 p-4 w-full shadow">
                         <a class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src={laprscpyIcons}/>
+                            <img alt="ecommerce" class="object-cover object-center w-36 h-36 block" src={hearticon} />
                         </a>
                         <div class="mt-4">
+                            <h2 class="text-gray-900 title-font text-lg font-medium">Breast Clinic </h2>
                             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
                         </div>
                     </div>
-                    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <div class="lg:w-1/4 m-4 md:w-1/2 p-4 w-full shadow">
                         <a class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src={laprscpyIcons}/>
+                            <img alt="ecommerce" class="object-cover object-center w-36 h-36 block" src={hearticon} />
                         </a>
                         <div class="mt-4">
+                            <h2 class="text-gray-900 title-font text-lg font-medium">General Surgery</h2>
                             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
                         </div>
                     </div>
-                    <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                        <a class="block relative h-48 rounded overflow-hidden">
-                            <img alt="ecommerce" class="object-cover object-center w-full h-full block" src={laprscpyIcons}/>
+                    <div class="lg:w-1/4 m-4 md:w-1/2 p-4 w-full shadow">
+                        <a class="block relative text-center h-48 rounded overflow-hidden">
+                            <img alt="ecommerce" class="object-cover object-center w-36 h-36 block" src={hearticon} />
                         </a>
                         <div class="mt-4">
+                            <h2 class="text-gray-900 title-font text-lg font-medium">Surgical Oncology</h2>
                             <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                            <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
                         </div>
                     </div>
-                   
+
                 </div>
             </div>
         </section>
