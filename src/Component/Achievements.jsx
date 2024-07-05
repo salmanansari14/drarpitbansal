badbbcf37a798eb10fb80fe99e3da446df94d255
salman ansari <salmanansari1409@gmail.com>
2024-07-05 16:06:37 +0530

    addsomestyling

--- src/Component/Achievements.jsx
+++ src/Component/Achievements.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import samplearpit from '../assets/drapitPhotos/samplearpit.png'
 
 const Achievements = () => {
   return (
     <section id='achievements' class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto flex flex-wrap">
       <div class="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden">
-        <img alt="feature" class="object-cover object-center h-full w-full" src="src\assets\dr.apit photos\Picsart_23-12-13_14-31-38-767.jpg"/>
+        <img alt="feature" class="object-cover object-center h-full w-full" src={samplearpit}/>
       </div>
       <div class="flex flex-col flex-wrap lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
         <div class="flex flex-col mb-10 lg:items-start items-center">
