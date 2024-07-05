3a48e8d392704bf5e8a51284a067c8412b53b8bb
salman ansari <salmanansari1409@gmail.com>
2024-07-05 16:18:43 +0530

    adddropdownmeny

--- src/Component/NavigationB.jsx
+++ src/Component/NavigationB.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
-// import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-// import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 // import '../Style/Style.css'
 // import './Style.css'
 
@@ -128,6 +128,57 @@ function NavigationB() {
          <a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white-200 mr-4">
            Fourth Link
          </a>
+         
+                         <Menu as="div" className="relative inline-block text-left">
+                             <div>
+                                 <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                     Options
+                                     <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
+                                 </MenuButton>
+                             </div>
+
+                             <MenuItems
+                                 transition
+                                 className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                             >
+                                 <div className="py-1">
+                                     <MenuItem>
+                                         <a
+                                             href="#"
+                                             className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                                         >
+                                             Account settings
+                                         </a>
+                                     </MenuItem>
+                                     <MenuItem>
+                                         <a
+                                             href="#"
+                                             className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                                         >
+                                             Support
+                                         </a>
+                                     </MenuItem>
+                                     <MenuItem>
+                                         <a
+                                             href="#"
+                                             className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                                         >
+                                             License
+                                         </a>
+                                     </MenuItem>
+                                     <form action="#" method="POST">
+                                         <MenuItem>
+                                             <button
+                                                 type="submit"
+                                                 className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+                                             >
+                                                 Sign out
+                                             </button>
+                                         </MenuItem>
+                                     </form>
+                                 </div>
+                             </MenuItems>
+                         </Menu>
        </div>
        <div>
          <button className="inline-flex items-center bg-amber-500 border-0 py-2 px-4 text-white">
