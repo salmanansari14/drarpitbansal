2fb801728b075004f7ba275fbbb79f27a8d1f834
salman ansari <salmanansari1409@gmail.com>
2024-07-06 12:12:16 +0530

    styel

--- src/Component/Head.jsx
+++ src/Component/Head.jsx
@@ -7,7 +7,7 @@ const Head = () => {
         <NavigationB />
         <section class="text-gray-600 body-font bg-gradient-to-r from-blue-400">
             <div class="container mx-10 flex px-5 py-28 md:flex-row flex-col items-center">
-                <div class="lg:flex-grow md:w-1/2 lg:pl-24 pr-28 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <div class="lg:flex-grow md:w-1/2 lg:pl-24 pr-20 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                     <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Laparoscopic & Laser Surgery
                     </h1>
                     <p class="mb-8 leading-relaxed text-left">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
