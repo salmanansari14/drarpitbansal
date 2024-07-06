e733004626109e1ded3b58e6d44e1fb8c67410bf
salman ansari <salmanansari1409@gmail.com>
2024-07-06 12:11:11 +0530

    styel

--- src/Component/Head.jsx
+++ src/Component/Head.jsx
@@ -6,18 +6,17 @@ const Head = () => {
     return (<>
         <NavigationB />
         <section class="text-gray-600 body-font bg-gradient-to-r from-blue-400">
-            <div class="container mx-auto flex px-5 py-28 md:flex-row flex-col items-center">
-               
-                <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+            <div class="container mx-10 flex px-5 py-28 md:flex-row flex-col items-center">
+                <div class="lg:flex-grow md:w-1/2 lg:pl-24 pr-28 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                     <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Laparoscopic & Laser Surgery
                     </h1>
-                        {/* <span class="hidden lg:inline-block"/>Laparoscopic & Laser Surgeon<span/> */}
                     <p class="mb-8 leading-relaxed text-left">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-                    <div class="flex justify-center">
+                    {/* <div class="flex justify-center">
                         <button class="inline-flex text-white bg-blue-700 border-0 py-2 px-6 focus:outline-none hover:bg-blue-900 rounded text-lg">Book Appoinment</button>
                         <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Know me</button>
-                    </div>
+                    </div> */}
                 </div>
+
                 <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
                     <img class="object-cover object-center rounded-tl-full" alt="hero" src={LaparoscopicImage}/>
                 </div>
