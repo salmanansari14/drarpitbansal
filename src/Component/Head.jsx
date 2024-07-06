3378ab44c5fca297d0f8694e445576fdf4ddf1c6
salman ansari <salmanansari1409@gmail.com>
2024-07-06 11:27:42 +0530

    addfaq

--- src/Component/Head.jsx
+++ src/Component/Head.jsx
@@ -11,9 +11,9 @@ const Head = () => {
                     <img class="object-cover object-center rounded" alt="hero" src={arpitdr}/>
                 </div>
                 <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-                    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Dr. Arpit Bansal
-                        <br class="hidden lg:inline-block"/>Laparoscopy & Laser Surgeon
+                    <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Dr. Arpit Bansal | Laparoscopic & Laser Surgeon
                     </h1>
+                        {/* <span class="hidden lg:inline-block"/>Laparoscopic & Laser Surgeon<span/> */}
                     <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
                     <div class="flex justify-center">
                         <button class="inline-flex text-white bg-blue-700 border-0 py-2 px-6 focus:outline-none hover:bg-blue-900 rounded text-lg">Book Appoinment</button>
