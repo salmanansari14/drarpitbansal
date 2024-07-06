258325fee1172e344ce3c5a17f6adca9529d677b
salman ansari <salmanansari1409@gmail.com>
2024-07-06 12:04:33 +0530

    styel

--- src/Component/Head.jsx
+++ src/Component/Head.jsx
@@ -12,7 +12,7 @@ const Head = () => {
                     <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Laparoscopic & Laser Surgery
                     </h1>
                         {/* <span class="hidden lg:inline-block"/>Laparoscopic & Laser Surgeon<span/> */}
-                    <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                    <p class="mb-8 leading-relaxed text-left">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
                     <div class="flex justify-center">
                         <button class="inline-flex text-white bg-blue-700 border-0 py-2 px-6 focus:outline-none hover:bg-blue-900 rounded text-lg">Book Appoinment</button>
                         <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Know me</button>
